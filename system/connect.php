c5fc37d9e139737f808b497a9748fc48b08405bf
technet-systems <technet-systems@users.noreply.github.com>
2017-10-13 12:05:08 +0200

    Update connect.php

--- system/connect.php
+++ system/connect.php
@@ -1,5 +1,5 @@
 <?php
-$connect = mysql_connect("localhost", "technets_typer", "S15WLkDEJN") or die(mysql_error());
-$db = mysql_select_db('technets_typer', $connect) or die(mysql_error());
+$connect = mysql_connect("CHANGE_ME", "CHANGE_ME", "CHANGE_ME") or die(mysql_error());
+$db = mysql_select_db('CHANGE_ME', $connect) or die(mysql_error());
 mysql_query("SET NAMES 'utf8'");
 ?>
